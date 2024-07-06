9d7277a682bce985e883692aa75790626d91ac89
lokytech5 <lokytech5@gmail.com>
2024-07-06 11:26:03 +0100

    createUser endpoint working as expeceted with cognito

--- backend/src/utils/cognitoUtils.ts
+++ backend/src/utils/cognitoUtils.ts
@@ -6,6 +6,9 @@ const COGNITO_APP_CLIENT_ID = process.env.COGNITO_CLIENT_ID!;
 const COGNITO_APP_CLIENT_SECRET = process.env.COGNITO_CLIENT_SECRET!;
 
 export const getSecretHash = (username: string) => {
+  if (!COGNITO_APP_CLIENT_SECRET) {
+    throw new Error('COGNITO_APP_CLIENT_SECRET is not defined');
+  }
   const message = username + COGNITO_APP_CLIENT_ID;
   const hash = crypto.createHmac('sha256', COGNITO_APP_CLIENT_SECRET);
   hash.update(message);
