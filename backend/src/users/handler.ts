7a06a0beafce37b690c8dd2217bbfe9ee83240a6
lokytech5 <lokytech5@gmail.com>
2024-07-08 10:29:41 +0100

    added uplaod image endpoint and set up the neccessary permission in serverless.yml file and added event to save the url to dynamodb.

--- backend/src/users/handler.ts
+++ backend/src/users/handler.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import serverless from "serverless-http";
-import { createUser, verifyUser, loginUser, getUserById } from "./controller";
+import { createUser, verifyUser, loginUser, getUserById, uploadProfilePicture } from "./controller";
 import { validateToken } from "../middleware/validateToken";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
+const upload = multer();
 
 app.get("/users/:userId", validateToken, getUserById);
 app.post("/users", createUser);
 app.post("/users/verify", verifyUser);
+app.post("/users/upload-profile-picture", validateToken, upload.single('file'), uploadProfilePicture);
 app.post("/login", loginUser);
 
 
