397c4a6e9c4534badf4a4400932c4246c3cee498
lokytech5 <lokytech5@gmail.com>
2024-07-10 19:40:03 +0100

    remove unused import

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import HomePageBanner from './components/HomePageBanner'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import HomePageBanner from "./components/HomePageBanner";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
