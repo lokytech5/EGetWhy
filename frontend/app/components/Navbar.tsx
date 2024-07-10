14911d0a5eb2cfadfcd9d1dd0ea5a4ef5c4f1ade
lokytech5 <lokytech5@gmail.com>
2024-07-10 16:47:52 +0100

    Added navbar drawer to navbar component

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -3,10 +3,17 @@
 
  const Navbar = () => {
    return (
-     <div className="navbar bg-base-100">
-       <div className="navbar-start">
-         <div className="dropdown">
-           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+  <div className="navbar bg-base-200">
+  <div className="navbar-start">
+  <div>
+
+    {/* Navbar Drawer */}
+  <div className="drawer drawer-end">
+  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    {/* Page content here */}
+    <label htmlFor="my-drawer-4">   
+    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
              <svg
                xmlns="http://www.w3.org/2000/svg"
                className="h-5 w-5"
@@ -20,17 +27,31 @@
                  d="M4 6h16M4 12h8m-8 6h16" />
              </svg>
            </div>
-           <ul
-             tabIndex={0}
-             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+      
+    </label>
+  </div>
+  <div className="drawer-side z-50">
+    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col justify-center items-center space-y-4 no-underline">
+      {/* Sidebar content here */}
              <li><a href='/'>Home</a></li>
              <li><a href='/about'>About</a></li>
              <li><a href='/services'>Services</a></li>
              <li><a href='/contact'>Contact</a></li>
-           </ul>
+    </ul>
+  </div>
+</div>
+   {/* End of Navbar Drawer */}
+
+
          </div>
-         <a className="btn btn-ghost text-xl">daisyUI</a>
+         <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">EgetWhy</a>
+        </div>
        </div>
+
+
+
        <div className="navbar-center hidden lg:flex">
          <ul className="menu menu-horizontal px-1">
            <li><a href='/'>Home</a></li>
@@ -39,15 +60,38 @@
            <li><a href='/contact'>Contact</a></li>
          </ul>
        </div>
-       {/* Usermenu (profile section) */}
+       {/* Usermenu (Alert Section) */}
+       <div className="navbar-end">
+       <div className="dropdown dropdown-end">
+    <button className="btn btn-ghost btn-circle">
+      <div className="indicator">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+        </svg>
+        <span className="badge badge-xs badge-primary indicator-item"></span>
+      </div>
+    </button>
+       </div>
+
+
+       {/* (profile section) */}
        <div className="dropdown dropdown-end sm:block px-2">
       <button tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <Image
             alt="User Profile"
             src="/images/avatar.svg"
-            width={40} // Add the width of the image
-            height={40} // Add the height of the image
+            width={40}
+            height={40}
             className="rounded-full bg-white"
           />
         </div>
@@ -68,6 +112,7 @@
     </div>
 
        {/* End of (profile section) */}
+       </div>
      </div>
    )
  }
