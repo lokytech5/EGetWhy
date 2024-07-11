d90cfdccd0616e52e65e5b1962adab509848b0d0
lokytech5 <lokytech5@gmail.com>
2024-07-11 09:11:00 +0100

    add uuid to handle id in dynamodb, include infrastructure code in severless.yml file and directly use event for apigateway to avoid using express routing

--- backend/services/post-api/src/handler.ts
+++ backend/services/post-api/src/handler.ts
@@ -1,17 +1,58 @@
-import express from "express";
-import serverless from "serverless-http";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { docClient } from "../../../lib/dynamoClient"; // Adjust the path according to your project structure
+import { v1 as uuidv1 } from 'uuid';
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
 
+export const createPost = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // Check if event.body is null or undefined
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Missing request body' }),
+    };
+  }
 
-const app = express();
+  const data = JSON.parse(event.body);
+  const timestamp = new Date().toISOString();
 
-app.use(express.json());
+  if (!data.userID || !data.content) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Missing required fields' }),
+    };
+  }
 
-// app.post("/auth/signup", createUser);
-// app.post("/auth/login", loginUser);
-// app.post("/auth/verify", verifyUser);
+  const params = {
+    TableName: process.env.POSTS_TABLE,
+    Item: {
+      PostID: uuidv1(),
+      UserID: data.userID,
+      Content: data.content,
+      Hashtags: data.hashtags || [],
+      CategoryID: data.categoryID || null,
+      IsAnonymous: data.isAnonymous || false,
+      CreatedAt: timestamp,
+      UpdatedAt: timestamp,
+    },
+  };
 
-app.use((req, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
-
-export const handler = serverless(app);
+  try {
+    const command = new PutCommand(params);
+    await docClient.send(command);
+    return {
+      statusCode: 201,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: `Error creating post: ${error.message}` }),
+      };
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unknown error occurred' }),
+    };
+  }
+};
