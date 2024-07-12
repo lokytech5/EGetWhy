15bc746d751cfabb7d288e9ec7c2ead10bce2de2
lokytech5 <lokytech5@gmail.com>
2024-07-13 00:49:33 +0100

    move userMenu from navbar to its own component

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Image from "next/image";
+import UserMenu from "./UserMenu";
+
 
 const Navbar = () => {
   return (
@@ -87,39 +88,7 @@ const Navbar = () => {
             </div>
           </button>
         </div>
-
-        {/* (profile section) */}
-        <div className="dropdown dropdown-end sm:block px-2">
-          <button
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <Image
-                alt="User Profile"
-                src="/images/avatar.svg"
-                width={40}
-                height={40}
-                className="rounded-full bg-white"
-              />
-            </div>
-          </button>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a href="#" className="justify-between">
-                Profile
-              </a>
-            </li>
-            <li>
-              <a href="#">Settings</a>
-            </li>
-          </ul>
-        </div>
-        {/* End of (profile section) */}
+        <UserMenu/>
       </div>
     </div>
   );
---
+++ frontend/app/components/UserMenu.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from "next/image";
+
+
+const UserMenu = () => {
+  return (
+    <div className="dropdown dropdown-end sm:block px-2">
+    <button
+      tabIndex={0}
+      role="button"
+      className="btn btn-ghost btn-circle avatar"
+    >
+      <div className="w-10 rounded-full">
+        <Image
+          alt="User Profile"
+          src="/images/avatar.svg"
+          width={40}
+          height={40}
+          className="rounded-full bg-white"
+        />
+      </div>
+    </button>
+    <ul
+      tabIndex={0}
+      className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    >
+      <li>
+        <a href="#" className="justify-between">
+          Profile
+        </a>
+      </li>
+    </ul>
+  </div>
+  )
+}
+
+export default UserMenu
