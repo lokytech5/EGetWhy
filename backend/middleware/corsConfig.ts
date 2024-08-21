5252fe45a7435bd87b29b1f0be91b2193771a77f
lokytech5 <lokytech5@gmail.com>
2024-08-21 08:03:40 +0100

    fix middy timeout error.

--- backend/middleware/corsConfig.ts
+++ backend/middleware/corsConfig.ts
@@ -2,7 +2,9 @@ import middy from "@middy/core";
 import httpCors from "@middy/http-cors";
 
 export const applyMiddleware = (handler: any, origin: string) => {
-    return middy(handler).use(httpCors({
+    return middy(handler, {
+        timeoutEarlyInMillis: 0,
+    }).use(httpCors({
         origin: origin,
         credentials: true,
         headers: "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
