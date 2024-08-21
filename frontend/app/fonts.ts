135cf0190295650272e1519b606f48a18b8bb050
lokytech5 <lokytech5@gmail.com>
2024-08-21 11:30:00 +0100

    added custom fonts customization code to fonts.ts file

---
+++ frontend/app/fonts.ts
@@ -0,0 +1,20 @@
+import {Montserrat, Roboto, Poppins } from 'next/font/google';
+
+export const montserrat = Montserrat({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['400', '700'],
+})
+
+export const poppins = Poppins({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['400', '700'],
+})
+
+export const roboto = Roboto({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['400', '700', '900'],
+})
+
